2f9034ce68d49e419596ea574ec1e14f0a32d936
Adil Enver <adiljan.anwar@gmail.com>
2022-01-11 00:58:43 +0300

    Calculates Taxi Fare

---
+++ .idea/Patika-Java-101/src/taxiFareCalculater/Main.java
@@ -0,0 +1,20 @@
+package taxiFareCalculater;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20, startPrice = 10, totalPrice;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the distance in km: ");
+        km = scan.nextInt();
+
+        totalPrice = km * perKm;
+        totalPrice += startPrice;
+
+        totalPrice = (totalPrice < 20)? 20 :totalPrice;
+        System.out.println("Total price is: " + totalPrice);
+    }
+}
