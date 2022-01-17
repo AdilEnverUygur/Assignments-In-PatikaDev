8f818be00d4a0dffcb6ec146a73c14d8aa66729a
Adil Enver <adiljan.anwar@gmail.com>
2022-01-17 14:08:25 +0300

    Suggesting Events Based on air temperature

---
+++ .idea/Patika-Java-101/src/suggestingEventsBasedOnAirTemperature/Main.java
@@ -0,0 +1,26 @@
+package suggestingEventsBasedOnAirTemperature;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a heat temperature: ");
+        heat = scanner.nextInt();
+
+        if (heat < 5){
+            System.out.println("You can ski");
+        } else if (heat <= 25) {
+            if (heat <= 15){
+                System.out.println("Go to sinema");
+            }else if (heat >= 10){
+                System.out.println("Go to picnic");
+            }
+        }else {
+            System.out.println("Go to swimming");
+        }
+
+    }
+}
